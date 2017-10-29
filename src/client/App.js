15f33307f7070d6472f0359b88cb673a52b1c062
UHanczar <u.hanczar@gmail.com>
2017-10-29 20:22:00 +0300

    Adding_redux

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,20 @@
 // @flow
 
 import React from 'react';
+import HelloButton from './containers/HelloButton';
+import HelloAsyncButton from './containers/HelloAsyncButton';
+import Message from './containers/Message';
+import MessageAsync from './containers/MessageAsync';
+import { APP_NAME } from '../shared/config';
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => (
+  <div>
+    <h1>{APP_NAME}</h1>
+    <Message />
+    <HelloButton />
+    <MessageAsync />
+   <HelloAsyncButton />
+  </div>
+);
 
 export default App;
